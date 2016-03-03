3499b99d9652d63dd42eeb6c398d4f09dbb55549
leonhanxl <xiaoliang.han@maixunbytes.com>
2016-03-03 23:17:58 +0800

    Fix stack

--- Introduction-to-Algorithms/stack.py
+++ Introduction-to-Algorithms/stack.py
@@ -1,9 +1,10 @@
 
 
 class Stack(object):
-    def __init__(self):
-        self._stack = []
+    def __init__(self, size):
+        self._stack = [None for i in range(size)]
         self.top = 0
+        self.size = size
 
     def empty(self):
         if self.top == 0:
@@ -11,8 +12,16 @@ class Stack(object):
         else:
             return False
 
+    def full(self):
+        if self.top == self.size:
+            return True
+        else:
+            return False
+
     def push(self, value):
-        self._stack.append(value)
+        if self.full():
+            raise ValueError
+        self._stack[self.top] = value
         self.top += 1
 
     def pop(self):
@@ -24,12 +33,15 @@ class Stack(object):
             del self._stack[self.top]
             return value
 
+    def show(self):
+        return [v for v in self._stack if v is not None]
+
 
 if __name__ == '__main__':
-    s = Stack()
+    s = Stack(10)
     print s.empty()
     s.push(1)
     s.push(2)
-    print s._stack
+    print s.show()
     print s.pop()
-    print s._stack
+    print s.show()

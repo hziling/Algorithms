c6173da18126d4708b58a2c8fc618df13c9a0c69
leonhanxl <xiaoliang.han@maixunbytes.com>
2016-03-07 23:08:09 +0800

    Add rb-tree delete

--- Introduction-to-Algorithms/black_red_tree.py
+++ Introduction-to-Algorithms/black_red_tree.py
@@ -1,3 +1,4 @@
+from tree import tree_minimum
 
 
 def left_rotate(T, x):
@@ -84,6 +85,70 @@ def rb_insert_fixup(T, z):
     T.root.color = 'BLACK'
 
 
+def rb_transplant(T, u, v):
+    if u.parent is None:
+        T.root = v
+    elif u == u.parent.left:
+        u.parent.left = v
+    else:
+        u.parent.right = v
+
+    v.parent = u.parent
 
 
+def rb_delete(T, z):
+    y = z
+    y_original_color = y.color
+    if z.left is None:
+        x = z.right
+        rb_transplant(T, z, z.right)
+    elif z.right is None:
+        x = z.left
+        rb_transplant(T, z, z.left)
+    else:
+        y = tree_minimum(z.right)
+        y_original_color = y.color
+        x = y.right
+        if y.parent == z:
+            x.parent = y
+        else:
+            rb_transplant(T, y, y.right)
+            y.right = z.right
+            y.right.parent = y
+        rb_transplant(T, z, y)
+        y.left = z.left
+        y.left.parent = y
+        y.color = z.color
+    if y_original_color == 'BLACK':
+        rb_delete_fixup(T, x)
 
+
+def rb_delete_fixup(T, x):
+    while x != T.root and x.color == 'BLACK':
+        if x == x.parent.left:
+            w = x.parent.right
+            if w.color == 'RED':
+                w.color = 'BLACK'
+                x.parent.color = 'RED'
+                left_rotate(T, x.parent)
+                w = x.parent.right
+            if w.left.color == 'BLACK' and w.right.color == 'BLACK':
+                w.color = 'RED'
+                x = x.parent
+            else:
+                if w.right.color == 'BLACK':
+                    w.left.color = 'BLACK'
+                    w.color = 'RED'
+                    right_rotate(T, w)
+                    w = x.parent.right
+                w.color = x.parent.color
+                x.parent.color = 'BLACK'
+                w.right.color = 'BLACK'
+                left_rotate(T, x.parent)
+                x = T.root
+        else:
+            # right and left exchanged
+            pass
+
+    x.color = 'BLACK'
+    
